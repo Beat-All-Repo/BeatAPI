00f1a6ea2919126aa6ba1e3396dc88244d3c0f71
Beat-All-Repo <beatanihindistreamhub@gmail.com>
2026-04-30 08:17:42 +0530

    Domain, UA, +12 browser headers, timeout 15s Fix Adaptation

--- src/vendor/aniwatch/config/client.ts
+++ src/vendor/aniwatch/config/client.ts
@@ -7,14 +7,27 @@ import {
     ACCEPT_ENCODING_HEADER,
     USER_AGENT_HEADER,
     ACCEPT_HEADER,
+    SEC_CH_UA_HEADER,
+    ACCEPT_LANGUAGE_HEADER,
 } from "../utils/constants.js";
 
 const clientConfig = {
-    timeout: 8000,
+    timeout: 15000,
     headers: {
         Accept: ACCEPT_HEADER,
         "User-Agent": USER_AGENT_HEADER,
         "Accept-Encoding": ACCEPT_ENCODING_HEADER,
+        "Accept-Language": ACCEPT_LANGUAGE_HEADER,
+        "Cache-Control": "no-cache",
+        "Pragma": "no-cache",
+        "sec-ch-ua": SEC_CH_UA_HEADER,
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
     },
 };
 
